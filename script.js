ba29ddf701de783419d4b17cea655c19b25b5f31
MSebb23 <matteosabbatini23@gmail.com>
2021-03-26 00:34:22 +0100

    sistemato css, aggiunto alt sincronizzato

--- script.js
+++ script.js
@@ -3,11 +3,29 @@ var vue_box = new Vue (
         el : "#vue_box",
         data :{
             images : [
-                "https://awardsradar.com/wp-content/uploads/2020/09/image-39.jpeg",
-                "https://media.ktvb.com/assets/ETONLINE/images/fd91eab1-d45d-4662-8014-744dc44c9b9c/fd91eab1-d45d-4662-8014-744dc44c9b9c_1140x641.png",
-                "https://www.etonline.com/sites/default/files/styles/970xh/public/images/2020-01/sherry_pie_0.jpg?itok=t6WfYz1v",
-                "https://www.etonline.com/sites/default/files/styles/970xh/public/images/2020-01/brita_0.jpg?itok=3MdpDLgE",
-                "https://lh3.googleusercontent.com/proxy/qhsnWBjPAa6tS6tWJ723Darfueym1QxrM45nhvsN-n8qAqHusEb1FORalk2iB8D4vbpgsHFMcCHote0lgpet4Pk8kzNYHq1vp8Ly4WPJWG_TH27C5WKodKZ1LBYWnMOEj14VXw"
+                "img/penguin.jpg",
+                "img/iceland.jpg",
+                "img/fog.jpg",
+                "img/mountain.jpg",
+                "img/fox.jpg",
+                "img/tiger.jpg",
+                "img/aurora.jpg",
+                "img/treebamboo.jpg",
+                "img/iceberg.jpg",
+                "img/waterfall.jpg",
+                "img/octopus.jpg"
+            ],
+            alt : [
+                "foto di un pinguino",
+                "paesaggio innevato",
+                "colline nebbiose al mattino",
+                "volpe tra il vento",
+                "tigre che abbraccia un albero",
+                "aurora boreale nordica",
+                "albero che si interseca in una piantagione di bambu",
+                "rare foto di iceberg",
+                "cascata naturale",
+                "polpo che si nasconde all'interno di una conchiglia"
             ],
             img_index: 0
         },
